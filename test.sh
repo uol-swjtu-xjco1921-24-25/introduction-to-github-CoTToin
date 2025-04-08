b7d2abc510240a0d685bfbc28ee4e9a229348b1d
CoTToin <3388958887@qq.com>
2025-04-08 03:13:16 +0000

    test

--- test.sh
+++ test.sh
@@ -39,3 +39,34 @@ def is_valid_file(file_path):
     if start_count != 1 or end_count != 1:
         return False
     return True
+
+def run_maze_test(filename, inputs=None):
+    """
+    运行迷宫程序并捕获其输出和退出状态码
+    :param filename: 迷宫测试文件的名称
+    :param inputs: 可选参数，传入迷宫程序的输入指令列表
+    :return: 一个元组，包含程序的标准输出、标准错误输出和退出状态码
+    """
+    cmd = [MAZE_EXECUTABLE, os.path.join(TEST_DATA_DIR, filename)]
+    try:
+        proc = subprocess.Popen(
+            cmd,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+
+    if inputs:
+            input_str = "\n".join(inputs) + "\n"
+            stdout, stderr = proc.communicate(input_str)
+        else:
+            stdout, stderr = proc.communicate(
+            return_code = proc.returncode
+        return stdout, stderr, return_code
+    except FileNotFoundError:
+        print(f"错误: 未找到迷宫可执行文件 {MAZE_EXECUTABLE}")
+        return "", f"错误: 未找到迷宫可执行文件 {MAZE_EXECUTABLE}", 1
+    except Exception as e:
+        print(f"运行迷宫程序时发生未知错误: {e}")
+        return "", f"运行迷宫程序时发生未知错误: {e}", 1
